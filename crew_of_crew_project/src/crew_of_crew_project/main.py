90ee27519bde341cf9334412a0b774555a46756d
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-17 09:11:33 -0300

    feat: prompt ok

--- crew_of_crew_project/src/crew_of_crew_project/main.py
+++ crew_of_crew_project/src/crew_of_crew_project/main.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
 import sys
 import warnings
-
+from crewai import agent
+from dotenv import load_dotenv, find_dotenv
+import agentops
+import os
 from datetime import datetime
+import openai
+from crew import CrewOfCrewProject
 
-from crew_of_crew_project.crew import CrewOfCrewProject
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv('OPENAI_API_KEY')
+agentops_api_key = os.getenv('AGENTOPS_API_KEY')
+agentops.init(api_key=agentops_api_key, default_tags=['crewai-simple'])
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+SISTEMA = '''Um sistema para criação de uma crew que desenvolve
+conteudo para cursos em python. Primeiro faz uma pesquisa de conteúdos 
+relevantes, levanta tópicos importante dentro desse conteúdo, desenvolve
+um escopo, redige a apostila do curso e revisa'''
 
 def run():
     """
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
+        'definicao_do_sistema': SISTEMA,
         'current_year': str(datetime.now().year)
     }
     
@@ -64,3 +72,6 @@ def test():
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    run()
