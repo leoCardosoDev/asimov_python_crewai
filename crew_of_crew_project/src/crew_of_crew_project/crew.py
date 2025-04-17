2aeb06d9793ecce116e7188389d58cd5bbcee306
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-17 09:05:10 -0300

    feat: adjust agents and tasks

--- crew_of_crew_project/src/crew_of_crew_project/crew.py
+++ crew_of_crew_project/src/crew_of_crew_project/crew.py
@@ -1,76 +1,84 @@
+from dotenv import load_dotenv
+_ = load_dotenv()
+
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 
+import agentops
+agentops.init()
+
 @CrewBase
-class CrewOfCrewProject():
-    """CrewOfCrewProject crew"""
-    agents_config = 'config/agents.yaml'
-    tasks_config = 'config/tasks.yaml'
+class CreateCrewProject():
+	"""CreateCrewProject crew"""
 
-    @agent
-    def arquiteto_de_sistemas(self) -> Agent:
-        return Agent(
-            config=self.agents_config['arquiteto_de_sistemas'],
-            verbose=True
-        )
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
 
-    @agent
-    def engenheiro_de_ia(self) -> Agent:
-        return Agent(
-            config=self.agents_config['engenheiro_de_ia'],
-            verbose=True
-        )
+	@agent
+	def arquiteto_de_solucao(self) -> Agent:
+		return Agent(
+			config=self.agents_config['arquiteto_de_solucao'],
+			verbose=True,
+		)
 
-    @agent
-    def desenvolvedor_backend(self) -> Agent:
-        return Agent(
-            config=self.agents_config['desenvolvedor_backend'],
-            verbose=True
-        )
+	@agent
+	def especialista_em_agentes(self) -> Agent:
+		return Agent(
+			config=self.agents_config['especialista_em_agentes'],
+			verbose=True
+		)
+	
+	@agent
+	def gerente_de_fluxo_de_trabalho(self) -> Agent:
+		return Agent(
+			config=self.agents_config['gerente_de_fluxo_de_trabalho'],
+			verbose=True
+		)
 
-    @agent
-    def engenheiro_de_qa(self) -> Agent:
-        return Agent(
-            config=self.agents_config['engenheiro_de_qa'],
-            verbose=True
-        )
+	@agent
+	def engenheiro_de_orquestracao(self) -> Agent:
+		return Agent(
+			config=self.agents_config['engenheiro_de_orquestracao'],
+			verbose=True
+		)
 
-    @task
-    def especificar_requisitos_sistema_multi_agents(self) -> Task:
-        return Task(
-            config=self.tasks_config['especificar_requisitos_sistema_multi_agents'],
-        )
 
-    @task
-    def modelar_arquitetura_multi_agents(self) -> Task:
-        return Task(
-            config=self.tasks_config['modelar_arquitetura_multi_agents'],
-        )
+	@task
+	def planejar_arquitetura_do_sistema(self) -> Task:
+		return Task(
+			config=self.tasks_config['planejar_arquitetura_do_sistema'],
+		)
 
-    @task
-    def definir_documentar_agentes(self) -> Task:
-        return Task(
-            config=self.tasks_config['definir_documentar_agentes'],
-        )
+	@task
+	def criar_os_agentes(self) -> Task:
+		return Task(
+			config=self.tasks_config['criar_os_agentes'],
+			output_file='output/agents.yaml'
+		)
 
-    @task
-    def elaborar_tasks_e_workflows(self) -> Task:
-        return Task(
-            config=self.tasks_config['elaborar_tasks_e_workflows'],
-        )
+	@task
+	def definir_as_tarefas(self) -> Task:
+		return Task(
+			config=self.tasks_config['definir_as_tarefas'],
+			context=[self.planejar_arquitetura_do_sistema()],
+			output_file='output/tasks.yaml'
+		)
 
-    @task
-    def implementar_crew_py_e_integracoes(self) -> Task:
-        return Task(
-            config=self.tasks_config['implementar_crew_py_e_integracoes'],
-        )
+	@task
+	def desenvolver_o_arquivo_de_orquestracao(self) -> Task:
+		return Task(
+			config=self.tasks_config['desenvolver_o_arquivo_de_orquestracao'],
+			context=[self.planejar_arquitetura_do_sistema(), self.criar_os_agentes()],
+			output_file='output/crew.py'
+		)
 
-    @crew
-    def crew(self) -> Crew:
-        """Creates the CrewOfCrewProject crew"""
-        return Crew(
-            agents=self.agents, 
-            tasks=self.tasks,
-            process=Process.sequential,
-            verbose=True,
-        )
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the CreateCrewProject crew"""
+
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+		)
